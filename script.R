37d99daab7ad9ec7cd239431fee11ddebdc20926
Indrit Kello <indritkello97@gmail.com>
2020-03-29 23:57:28 +0200

    Update script.R

--- script.R
+++ script.R
@@ -1,17 +1,18 @@
 #IMPORT DATASET
 read.csv("C:/Users/User/Documents/R-Studies/Housing.csv")->houses
-str(houses)
-summary(houses)
+str(houses) # Display the content of the file
+summary(houses) #Data Statistics
 
 #Install "dplyr" package and use it
-library(dplyr)
+library(dplyr) # Library for Data Manipulation
 
 ### DATA CLEANING (pipeline operation)
 houses %>% select(c(-1))->houses
+str(houses)
 houses %>% select(-driveway)->houses
 
 ##Install "ggplot2" package and use it
-library(ggplot2)
+library(ggplot2) # Library for DATA VISUALIZATION
 
 ### DATA VISUALIZATION
 #simple plots
@@ -21,39 +22,42 @@ ggplot(data=houses, aes(x=price))+geom_histogram(bins=40, fill="lightblue", col=
 #yes/no records for driveway column
 ggplot(data=houses, aes(y=price, x=driveway, fill=driveway))+geom_boxplot()
 
+
 #lotsize with price correlation
-ggplot(houses, aes(x=lotsize, y=price))+geom_point(col="purple")+geom_smooth(method="lm", se=F)
+ggplot(houses, aes(x=lotsize, y=price))+geom_point(col="purple")+geom_smooth(method="lm", se = F)
+# lm -> Linear Model se -> square root of variance/ no of observations (T/F)
 
 #3 factor plot
 ggplot(houses, aes(x=lotsize, y=price, col=factor(bedrooms)))+geom_point()+geom_smooth(method="lm", se=F)
 
 ### SPLITTING DATA (Training & Testing set)
-library(caTools)
+library(caTools) # Library for window statistic functions
 
-
-houses<-subset(houses, select = c(-recroom))
-houses<-subset(houses, select = c(-fullbase))
-houses<-subset(houses, select = c(-gashw))
-houses<-subset(houses, select = c(-airco))
+#skip columns you dont want to be part of the model, or lease the set as it is
 houses<-subset(houses, select = c(-prefarea))
 
-sample.split(houses$price, SplitRatio=0.65)->split_index
-train<-subset(houses, split_index==T)
+sample.split(houses$price, SplitRatio=0.7)->split_index # 0.65 of the records will have the splitIndex = T
+train<-subset(houses, split_index==T) # Those with index = T are moved to subset of training
 test<-subset(houses, split_index==F)
 nrow(train)
 nrow(test)
+summary(train)
 
 ### MODEL BUILDING (LINEAR REGRESSION)
-lm(price~.,data=train)->mod1
-predict(mod1, test)->result
-cbind(actual=test$price, predicted=result)->compare_result
-as.data.frame(compare_result)->compare_result
+lm(price~.,data=train)->mod1 #linear model of price towards all other attributes
+predict(mod1, test)->result #The first attribute should be a model
+cbind(actual=test$price, predicted=result)->compare_result #Compares actual price values from test subset agains predicted ones
+as.data.frame(compare_result)->compare_result #Set of functions/pattern which can be enhanced
+
 compare_result$actual-compare_result$predicted->error
 
 #investigate the impact (look at the stars)
 summary(mod1)
 
 #calculate the error
-cbind(compare_result, error)
-sqrt(mean(compare_result$error^2))->rmse1
+cbind(compare_result, error)->compare_result# binds the error column in compare_result set
 
+# **Root Mean Square Error (RMSE) is the standard deviation of the residuals (prediction errors). 
+# **Residuals are a measure of how far from the regression line data points are;
+sqrt(mean(compare_result$error^2))->rmse1
+str(rmse1)
